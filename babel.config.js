d82e9bd5543069ca565617e2e58b6811c3a88480
包子约翰 <zhouhan@yunjisoft.com>
2025-05-30 20:11:10 +0800

    移除不需要的babel依赖

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,4 @@
 module.exports = {
-  plugins: ['@babel/plugin-transform-arrow-functions'],
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          chrome: '60',
-          firefox: '60',
-          ie: '11',
-        },
-        corejs: 3,
-        useBuiltIns: 'usage',
-      },
-    ],
-  ],
-}
+    plugins: [
+    ]
+};
